05b5033335ece785ad05a3eec5127b8223e9e1fb
Steve Jobsnihack <136087495+SteveJobsnihack@users.noreply.github.com>
2023-08-13 11:15:02 +0200

    Update script

--- app.py
+++ app.py
@@ -1,74 +1,73 @@
 import requests
-import csv
+import json
+import random
 
-# Get your API key here: https://scraptubes.de
-scraptubes_apikey = "ENTER API KEY HERE"
+# Get your personal ScrapTubes Key from https://scraptubes.de
+scraptubes_key = "ENTER YOUR KEY HERE"
 
-# Use ScrapTubes "TikTok User ID" under Scrape API (TikTok) if you need to look it up
-user_id = "ENTER USER ID HERE"
+url = "https://scraptubes.p.rapidapi.com/register/device"
+
+payload = { "proxy": "" }
+headers = {
+	"content-type": "application/json",
+	"Content-Type": "application/json",
+	"X-RapidAPI-Key": scraptubes_key,
+	"X-RapidAPI-Host": "scraptubes.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+device = response.json()
+iid = device["Install id"]
+did = device["Device id"]
+openudid = device["Openudid"]
+cdid = device["Cdid"]
+
+def scrape_followers(userid: str):
+    try:
+        max_time = 0
+        for i in range(0, 500):
+            try:
+                url = "https://scraptubes.p.rapidapi.com/scrape/tiktok/user/followers"
+
+                payload = json.dumps({
+                    "iid": str(iid),
+                    "did": str(did),
+                    "openudid": str(openudid),
+                    "cdid": str(cdid),
+                    "userid": userid,
+                    "count": "10",
+                    "maxtime": f'{str(max_time)}',
+                    "cookie": "",
+                    "proxy": ""
+                })
+                headers = {
+                    "content-type": "application/json",
+	                "Content-Type": "application/json",
+	                "X-RapidAPI-Key": scraptubes_key,
+	                "X-RapidAPI-Host": "scraptubes.p.rapidapi.com"
+                }
+
+                result = requests.request("POST", url, headers=headers, data=payload)
+                data = result.json()
+
+                for follower in data['followers']:
+                    print('Nickname: {}'.format(follower['nickname']))
+
+                    with open('users.txt', 'a', encoding='utf-8') as f:
+                        f.write('{}\n'.format(follower['nickname']))
+
+                if data['has_more']:
+                    max_time = data['min_time']
+                else:
+                    break
+            except:
+                pass
+    except:
+        print(f'[-] Scraping exception!')
 
 
-fieldnames = [
-    'unique_id',
-    'uid',
-    'region',
-    'language',
-    'following_count',
-    'follower_count',
-    'favoriting_count',
-    'ins_id',
-    'youtube_channel_id',
-    'youtube_channel_title',
-    'twitter_id',
-    'twitter_name'
-]
+profiles = ["6802299750194643973"]
 
-with open(r'data.csv', 'w', encoding="utf-8") as f:
-    writer = csv.writer(f)
-    row = {}
-    for x in fieldnames:
-        row[x] = x
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writerow(row)
-
-def get_followers(max_time):
-    url = "https://scraptubes.p.rapidapi.com/scrape/tiktok/user/followers"
-
-    payload = {
-        "iid": "7251149210593249030",
-        "did": "7251148112487138821",
-        "openudid": "262cbf92d8174603",
-        "cdid": "c83851b9-e08c-4ed1-a4ca-a55e81fc370b",
-        "userid": str(user_id),
-        "count": "30",
-        "maxtime": str(max_time),
-        "cookie": "",
-        "proxy": ""
-    }
-    headers = {
-        "content-type": "application/json",
-        "Content-Type": "application/json",
-        "X-RapidAPI-Key": scraptubes_apikey,
-        "X-RapidAPI-Host": "scraptubes.p.rapidapi.com"
-    }
-
-    r = requests.post(url, json=payload, headers=headers).json()
-
-    for u in r["followers"]:
-        with open(r'data.csv', 'a', encoding="utf-8") as f:
-            writer = csv.writer(f)
-            row = {}
-            for x in fieldnames:
-                if x in u and u[x]:
-                    row[x] = str(u[x])
-                            
-                    if x == "uid":
-                        row[x] = str(u[x]) + ''
-
-            writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(row)
-
-        if r["has_more"]:
-            get_followers(r["min_time"])
-
-get_followers(0)
+for p in profiles:
+    scrape_followers(p)
